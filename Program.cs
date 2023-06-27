8193feb0d6fdfad76188ab842efe9656cb66d49b
Patricio Monpelat <patricio.monpelat@gmail.com>
2023-06-27 12:12:26 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using WebAPI.Models;
+using WebAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDB"));
+builder.Services.AddSingleton<MongoDBService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "WEB API NET CORE 6");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Services/MongoDBService.cs
@@ -0,0 +1,42 @@
+﻿using WebAPI.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebAPI.Services
+{
+    public class MongoDBService
+    {
+        private readonly IMongoCollection<Playlist> _playlistCollection;
+
+        public MongoDBService(IOptions<MongoDBSettings> mongoDBSettings) {
+            MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI);
+            IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
+            _playlistCollection = database.GetCollection<Playlist>(mongoDBSettings.Value.CollectionName);
+        }
+
+        public async Task CreateAsync(Playlist playlist)
+        {
+            await _playlistCollection.InsertOneAsync(playlist);
+            return;
+        }
+
+        public async Task<List<Playlist>> GetAsync() { 
+            return await _playlistCollection.Find(new BsonDocument()).ToListAsync();
+        }
+
+        public async Task AddToPlaylistAssync(string id, string movieId) {
+            FilterDefinition<Playlist> filter = Builders<Playlist>.Filter.Eq("Id", id);
+            UpdateDefinition<Playlist> update = Builders<Playlist>.Update.AddToSet<string>("items", movieId);
+            await _playlistCollection.UpdateOneAsync(filter, update);
+            return;
+        }
+
+        public async Task DeleteAsync(string id) {
+            FilterDefinition<Playlist> filter = Builders<Playlist>.Filter.Eq("Id", id);
+            await _playlistCollection.DeleteOneAsync(filter);
+            return;
+        }
+    }
+}
